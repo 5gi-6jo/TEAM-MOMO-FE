7ded4e4f136791921a6bdaa0af0f879e26561ce8
5aro <hwalnut94@gmail.com>
2022-03-07 23:52:34 +0900

    [test] git actions

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React git actions
+          Learn React git actions test
         </a>
       </header>
     </div>
