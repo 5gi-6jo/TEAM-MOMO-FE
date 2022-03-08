cd58300f50d04855a9c34c8ec50b5306c6602a07
5aro <hwalnut94@gmail.com>
2022-03-08 13:47:03 +0900

    [test] master github actions

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React git actions test
+          Learn React auto deploy success!
         </a>
       </header>
     </div>
